26b6d6d7fc616ff5d8d3d4e2936603ef432b0f14
truesnow <>
2023-09-24 17:01:26 +0800

    edit profile

--- src/router/routes.js
+++ src/router/routes.js
@@ -18,4 +18,28 @@ export default [
     name: 'Login',
     component: () => import('@/views/auth/Login')
   },
+  {
+    path: '/users/1/edit',
+    component: () => import('@/views/users/Edit.vue'),
+    children: [
+      {
+        path: '',
+        name: 'EditProfile',
+        component: () => import('@/views/users/Profile.vue'),
+        meta: { auth: true }
+      },
+      {
+        path: '/users/1/edit_avatar',
+        name: 'EditAvatar',
+        component: () => import('@/views/users/Avatar.vue'),
+        meta: { auth: true }
+      },
+      {
+        path: '/users/1/edit_password',
+        name: 'EditPassword',
+        component: () => import('@/views/users/Password.vue'),
+        meta: { auth: true }
+      }
+    ]
+  },
 ]
