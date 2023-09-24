4a3c72a55ee4769fe085ce635cb11ea81cf82273
truesnow <>
2023-09-24 12:15:51 +0800

    register and login optimize

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,32 @@ const routes = [
     path: '/auth/register',
     name: 'Register',
     component: () => import('@/views/auth/Register')
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/Home')
+  },
+  {
+    path: '*',
+    // 重定向
+    redirect: '/'
   }
 ]
 
 const router = new Router({
   mode: 'history',
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const auth = router.app.$options.store.state.auth
+
+  if (auth && to.path.indexOf('/auth/') !== -1) {
+    next('/')
+  } else {
+    next()
+  }
 })
 
 export default router
