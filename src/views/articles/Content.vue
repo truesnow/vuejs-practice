dfa1cb1e46dede912ddc4c3924f44e350888fc57
truesnow <>
2023-10-27 21:56:36 +0800

    add reward modal

--- src/views/articles/Content.vue
+++ src/views/articles/Content.vue
@@ -25,6 +25,8 @@
           <a @click="like" href="javascript:;" class="vote btn btn-primary popover-with-html" :class="likeClass">
             <i class="fa fa-thumbs-up"></i> {{ likeClass ? '已赞' : '点赞' }}
           </a>
+          <div class="or"></div>
+          <button @click="showQrcode = true" class="btn btn-success"><i class="fa fa-heart"></i> 打赏</button>
         </div>
         <div class="voted-users">
           <div class="user-lists">
@@ -37,6 +39,26 @@
         </div>
       </div>
     </div>
+    
+    <Modal :show.sync="showQrcode" class="text-center">
+      <template v-slot:header>
+        <div v-if="user">
+          <img :src="user.avatar" class="img-thumbnail avatar" width="48">
+        </div>
+      </template>
+      <div>
+        <p class="text-md">如果你想学习更多前端的知识，Learnku Vue.js.com 是个不错的开始</p>
+        <div class="payment-qrcode inline-block">
+          <h5>扫一扫打开 Learnku Vue.js.com</h5>
+          <p><qrcode-vue value="https://learnku.com/vuejs/" :size="160"></qrcode-vue></p>
+        </div>
+      </div>
+      <template v-slot:footer>
+        <div>
+          <div class="text-center">祝你学习愉快 :)</div>
+        </div>
+      </template>
+    </Modal>
 
   </div>
 </template>
@@ -46,9 +68,13 @@ import SimpleMDE from 'simplemde'
 import hljs from 'highlight.js'
 import emoji from 'node-emoji'
 import { mapState } from 'vuex'
+import QrcodeVue from 'qrcode.vue'
 
 export default {
   name: 'Content',
+  components: {
+    QrcodeVue
+  },
   data() {
     return {
       title: '', // 文章标题
@@ -57,6 +83,7 @@ export default {
       uid: 1, // 用户 ID
       likeUsers: [], // 点赞用户列表
       likeClass: '', // 点赞样式
+      showQrcode: false, // 是否显示打赏弹窗
     }
   },
   computed: {
